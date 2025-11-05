5bee88f1d150feadf733b9f4cef8d8755aa3c52d
Aman singh rawat <ar6477594@gmail.com>
2025-11-05 09:38:02 +0530

    Add files via upload

---
+++ Button.jsx
@@ -0,0 +1,14 @@
+function Button(){
+    const Sty ={
+        width:"100px",
+        height:"50px",
+        background:"#000000",
+        border:"5px Solid #ffffff",
+    }
+    return(
+        <>
+        <button style={Sty}>Click</button>
+        </>
+    );
+}
+export default Button
